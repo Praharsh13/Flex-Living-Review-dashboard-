9084aeb57d7df7f87bbda36e345636f3a61fdec3
Praharsh Pranjal <ppjha.13@gmail.com>
2025-09-17 01:28:32 +0100

    Done

--- Frontend/src/lib/api.ts
+++ Frontend/src/lib/api.ts
@@ -1,5 +1,5 @@
 import type { ReviewsPayload } from "../types"
-
+const API =import.meta.env.VITE_API_URL
 
 function authHeaders(): HeadersInit {
     const t = localStorage.getItem('token')
@@ -8,7 +8,7 @@ function authHeaders(): HeadersInit {
   
 
   export async function login(email: string, password: string) {
-    const res = await fetch('/api/auth/login', {
+    const res = await fetch(`${API}/api/auth/login`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password })
@@ -19,7 +19,7 @@ function authHeaders(): HeadersInit {
 
 
 export let fetchReviews=async():Promise<ReviewsPayload>=>{
-    const res=await fetch(`/api/reviews/hostaway`,{
+    const res=await fetch(`{API}/api/reviews/hostaway`,{
         headers:authHeaders()
     })
     if(!res.ok){
@@ -34,7 +34,7 @@ export let fetchReviews=async():Promise<ReviewsPayload>=>{
 
 
 export let toggleAproval=async(id:string)=>{
-    const res=await fetch(`/api/reviews/approve/${id}`,{
+    const res=await fetch(`{API}/api/reviews/approve/${id}`,{
         method:'POST',
         headers:authHeaders()
     })
@@ -50,7 +50,7 @@ export let toggleAproval=async(id:string)=>{
 
 
 export async function fetchPublicReviews(): Promise<ReviewsPayload> {
-    const res = await fetch('/api/reviews/publicreviews')
+    const res = await fetch('{API}/api/reviews/publicreviews')
     if (!res.ok) throw new Error('Failed to fetch public reviews')
     return res.json()
   }
